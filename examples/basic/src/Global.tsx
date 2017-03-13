87746c3c3c3207909e469bbeca7259774e2b2e86
Jordan Schroter <jordan.schroter@gmail.com>
2017-03-13 15:32:31 -0400

    fixes global style ordering issue with glamor

--- examples/basic/src/Global.tsx
+++ examples/basic/src/Global.tsx
@@ -6,11 +6,22 @@ import { css } from 'glamor'
 
 import { preventDefault } from './domHelpers'
 
+let previousRule
+
 function createStyles(theme) {
-  css.global('html', {
+  const htmlCss = css({
     fontFamily: theme.fontFamily,
     fontSize: `${theme.baseFontSize}px`
   })
+
+  // fixes global style ordering issue with glamor
+  // https://github.com/threepointone/glamor/issues/222#issuecomment-286186869
+  const rule = Object.keys(htmlCss)[0]
+
+  document.documentElement.removeAttribute(previousRule)
+  document.documentElement.setAttribute(rule, htmlCss[rule])
+
+  previousRule = rule
 }
 
 function Global({ children = null }) {
